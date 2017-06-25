50d0ce1201f13a92ab98ef94d51cd62b9c87d00e
Julius Mittenzwei <julius@mittenzwei.com>
2017-06-25 12:35:56 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
         name='pyvlx',
         description='PyVLX - controling VELUX windows with Python via KLF 200',
 
-        version='0.1.0',
-        download_url='https://github.com/Julius2342/pyvlx/archive/0.1.0.zip',
+        version='0.1.1',
+        download_url='https://github.com/Julius2342/pyvlx/archive/0.1.1.zip',
         url='https://github.com/Julius2342/pyvlx',
 
         author='Julius Mittenzwei',
