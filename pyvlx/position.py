51d386646e6e86e50b20d7fbefe0d67b9e32c3b2
Julius Mittenzwei <julius@mittenzwei.com>
2018-11-19 23:40:54 +0100

    Position helper class.

---
+++ pyvlx/position.py
@@ -0,0 +1,95 @@
+"""Module for Position class."""
+from .exception import PyVLXException
+
+
+class Position():
+    """Class for storing a position."""
+
+    UNKNOWN_POSITION = b'\xF7\xFF'
+    MAX = 51200
+
+    def __init__(self, raw=None, position=None, position_percent=None):
+        """Initialize Position class."""
+        if raw is not None:
+            self.raw = self.from_raw(raw)
+        elif position is not None:
+            self.position = position
+        elif position_percent is not None:
+            self.position_percent = position_percent
+        else:
+            self.raw = Position.UNKNOWN_POSITION
+
+    @property
+    def known(self):
+        """Known property, true if position is not in an unknown position."""
+        return self.raw != Position.UNKNOWN_POSITION
+
+    @property
+    def position(self):
+        """Position property."""
+        return self.to_int(self.raw)
+
+    @position.setter
+    def position(self, position):
+        """Setter of internal raw via position."""
+        self.raw = self.from_int(position)
+
+    @property
+    def position_percent(self):
+        """Position percent property."""
+        return self.to_percent(self.raw)
+
+    @position_percent.setter
+    def position_percent(self, position_percent):
+        """Setter of internal raw via percent position."""
+        self.raw = self.from_percent(position_percent)
+
+    @staticmethod
+    def from_int(position):
+        """Create raw out of position vlaue."""
+        if not isinstance(position, int):
+            raise PyVLXException("Position::position_has_to_be_int")
+        if position < 0:
+            raise PyVLXException("Position::position_has_to_be_positive")
+        if position > Position.MAX:
+            raise PyVLXException("Position::position_out_of_range")
+        return bytes([position >> 8 & 255, position & 255])
+
+    @staticmethod
+    def to_int(raw):
+        """Create int position value out of raw."""
+        return raw[0] * 256 + raw[1]
+
+    @staticmethod
+    def from_percent(position_percent):
+        """Create raw value out of percent position."""
+        if not isinstance(position_percent, int):
+            raise PyVLXException("Position::position_percent_has_to_be_int")
+        if position_percent < 0:
+            raise PyVLXException("Position::position_percent_has_to_be_positive")
+        if position_percent > 100:
+            raise PyVLXException("Position::position_percent_out_of_range")
+        return bytes([position_percent*2, 0])
+
+    @staticmethod
+    def to_percent(raw):
+        """Create percent position value out of raw."""
+        # The first byte has the vlue from 0 to 200. Ignoring the second one.
+        return int(raw[0]/2)
+
+    @staticmethod
+    def from_raw(raw):
+        """Test if raw packets are valid for initialization of Position."""
+        if not isinstance(raw, bytes):
+            raise PyVLXException("Position::raw_must_be_bytes")
+        if len(raw) != 2:
+            raise PyVLXException("Position::raw_must_be_two_bytes")
+        if raw != Position.UNKNOWN_POSITION and Position.to_int(raw) > Position.MAX:
+            raise PyVLXException("position::raw_exceed_limit")
+        return raw
+
+    def __str__(self):
+        """Return string representation of object."""
+        if self.raw == Position.UNKNOWN_POSITION:
+            return "UNKNOWN"
+        return "{} %".format(self.position_percent)
---
+++ test/position_test.py
@@ -0,0 +1,71 @@
+"""Test for Position class."""
+import unittest
+from pyvlx.position import Position
+from pyvlx.exception import PyVLXException
+
+
+class TestSlip(unittest.TestCase):
+    """Test class for Position class."""
+
+    # pylint: disable=invalid-name
+
+    def test_no_position(self):
+        self.assertEqual(Position().raw, b'\xF7\xFF')
+
+    def test_raw(self):
+        """Test raw assignement."""
+        self.assertEqual(Position(raw=b'\x00\x00').raw, b'\x00\x00')
+        self.assertEqual(Position(raw=b'\x0A\x05').raw, b'\x0A\x05')
+        self.assertEqual(Position(raw=b'\xC8\x00').raw, b'\xC8\x00')
+
+    def test_position(self):
+        """Test initiaization with position value."""
+        self.assertEqual(Position(position=0).position, 0)
+        self.assertEqual(Position(position=10).position, 10)
+        self.assertEqual(Position(position=1234).position, 1234)
+        self.assertEqual(Position(position=12345).position, 12345)
+        self.assertEqual(Position(position=51200).position, 51200)
+
+    def test_percent(self):
+        """Test initialization with percent value."""
+        self.assertEqual(Position(position_percent=0).position_percent, 0)
+        self.assertEqual(Position(position_percent=5).position_percent, 5)
+        self.assertEqual(Position(position_percent=50).position_percent, 50)
+        self.assertEqual(Position(position_percent=95).position_percent, 95)
+        self.assertEqual(Position(position_percent=100).position_percent, 100)
+
+    def test_conversion(self):
+        """Test conversion from one form to other."""
+        self.assertEqual(Position(raw=b'\x0A\x05').position, 2565)
+        self.assertEqual(Position(position_percent=50).position, 25600)
+        self.assertEqual(Position(position=12345).position_percent, 24)
+
+    def test_exception(self):
+        """Test wrong initialization of Position."""
+        with self.assertRaises(PyVLXException):
+            Position(position=2.5)
+        with self.assertRaises(PyVLXException):
+            Position(position=-1)
+        with self.assertRaises(PyVLXException):
+            Position(position=51201)
+        with self.assertRaises(PyVLXException):
+            Position(position_percent=2.5)
+        with self.assertRaises(PyVLXException):
+            Position(position_percent=-1)
+        with self.assertRaises(PyVLXException):
+            Position(position_percent=101)
+        with self.assertRaises(PyVLXException):
+            Position(raw=b'\xC8\x01')
+        with self.assertRaises(PyVLXException):
+            Position(raw=b'\xC9\x00')
+ 
+    def test_known(self):
+        """Test 'known' property."""
+        self.assertTrue(Position(raw=b'\x12\x00').known)
+        self.assertTrue(Position(raw=b'\xC8\x00').known)
+        self.assertFalse(Position(raw=b'\xF7\xFF').known)
+
+    def test_str(self):
+        """Test string representation."""
+        self.assertEqual(str(Position(raw=b'\xF7\xFF')), "UNKNOWN")
+        self.assertEqual(str(Position(position_percent=50)), "50 %")
