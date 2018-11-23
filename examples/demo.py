678d1633fc690f18da7f653dad0eb15b3a597fdf
Julius Mittenzwei <julius@mittenzwei.com>
2018-11-23 09:07:57 +0100

    updated demo.py

--- examples/demo.py
+++ examples/demo.py
@@ -4,32 +4,50 @@ from pyvlx.config import Config
 from pyvlx.connection import Connection
 from pyvlx.login import Login
 from pyvlx.scene_list import SceneList
+from pyvlx.get_node_information import GetNodeInformation
+from pyvlx.get_all_nodes_information import GetAllNodesInformation
+
+
+async def frame_received_callback(frame):
+    """Log frame for better debugging."""
+    # print("Frame: ", frame)
+    # pylint: disable=unused-argument
+    pass
 
 
 async def demo(config, loop):
     """Demonstrate functionality of PyVLX."""
     connection = Connection(loop=loop, config=config)
+    connection.register_frame_received_cb(frame_received_callback)
     await connection.connect()
 
     login = Login(connection=connection, password=config.password)
     await login.do_api_call()
 
-    if login.success:
-        scene_list = SceneList(connection=connection)
-        await scene_list.do_api_call()
-        print(scene_list.scenes)
+    if not login.success:
+        print("Unable to login. Giving up""")
+        return
 
-        # from pyvlx.activate_scene import ActivateScene
-        # activate_scene = ActivateScene(connection=connection, scene_id=2)
-        # await activate_scene.do_api_call()
+    scene_list = SceneList(connection=connection)
+    await scene_list.do_api_call()
+    print(scene_list.scenes)
 
-        # activate_scene = ActivateScene(connection=connection, scene_id=0)
-        # await activate_scene.do_api_call()
-    # return
+    get_node_information = GetNodeInformation(connection=connection, node_id=6)
+    await get_node_information.do_api_call()
+    print(get_node_information.node)
 
-    from pyvlx.frame_get_all_nodes_information import FrameGetAllNodesInformationRequest
-    connection.write(FrameGetAllNodesInformationRequest())
-    await asyncio.sleep(10)
+    print("---------------------------------------")
+
+    get_all_nodes_information = GetAllNodesInformation(connection=connection)
+    await get_all_nodes_information.do_api_call()
+    print(get_all_nodes_information.nodes)
+
+    # from pyvlx.activate_scene import ActivateScene
+    # activate_scene = ActivateScene(connection=connection, scene_id=2)
+    # await activate_scene.do_api_call()
+
+    # activate_scene = ActivateScene(connection=connection, scene_id=0)
+    # await activate_scene.do_api_call()
 
     # BACKUP:
     #
@@ -45,27 +63,6 @@ async def demo(config, loop):
     # connection.write(FrameCommandSendRequest(node_ids=[3], position=100, session_id=8))
     # await asyncio.sleep(1)
     # connection.write(FrameCommandSendRequest(node_ids=[4], position=100, session_id=9))
-
-    # from pyvlx.frame_discover_nodes import FrameDiscoverNodesRequest
-    # connection.write(FrameDiscoverNodesRequest())
-
-    from pyvlx.frame_get_node_information import FrameGetNodeInformationRequest
-    # connection.write(FrameGetNodeInformationRequest(node_id=0))
-    # await asyncio.sleep(1)
-    # connection.write(FrameGetNodeInformationRequest(node_id=1))
-    # await asyncio.sleep(1)
-    # connection.write(FrameGetNodeInformationRequest(node_id=2))
-    # await asyncio.sleep(1)
-    # connection.write(FrameGetNodeInformationRequest(node_id=3))
-    # await asyncio.sleep(1)
-    # connection.write(FrameGetNodeInformationRequest(node_id=4))
-    # await asyncio.sleep(1)
-    # connection.write(FrameGetNodeInformationRequest(node_id=5))
-    # await asyncio.sleep(1)
-    # connection.write(FrameGetNodeInformationRequest(node_id=6))
-    await asyncio.sleep(1)
-    connection.write(FrameGetNodeInformationRequest(node_id=7))
-    await asyncio.sleep(5)
 
 
 async def main(loop):
