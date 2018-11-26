115bfb86632ac3922e5c236773c40a72eb70cc5c
Julius Mittenzwei <julius@mittenzwei.com>
2018-11-26 22:10:31 +0100

    Adding OpeningDevice to public interface

--- pyvlx/__init__.py
+++ pyvlx/__init__.py
@@ -4,6 +4,6 @@
 from .pyvlx import PyVLX
 from .exception import PyVLXException
 from .nodes import Nodes
-from .opening_device import Window, RollerShutter, Blind
+from .opening_device import OpeningDevice, Window, RollerShutter, Blind
 from .scenes import Scenes
 from .scene import Scene
