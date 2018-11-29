bb77cd246f4442e31222fb936017e5a518658af0
Julius Mittenzwei <julius@mittenzwei.com>
2018-11-29 22:33:36 +0100

    added long description to pypi package

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 """Module for setting up PyVLX pypi object."""
+import io
+import os
 
 from setuptools import setup, find_packages
 
@@ -6,7 +8,15 @@ REQUIRES = [
     'PyYAML'
 ]
 
+PKG_ROOT = os.path.dirname(__file__)
+
 VERSION = '0.2.2'
+
+# Error-handling here is to allow package to be built w/o README included
+try:
+    README = io.open(os.path.join(PKG_ROOT, 'README.md'), encoding='utf-8').read()
+except IOError:
+    README = ''
 
 setup(
     name='pyvlx',
@@ -16,6 +26,7 @@ setup(
     url='https://github.com/Julius2342/pyvlx',
 
     description="PyVLX is a wrapper for the Velux KLF 200 API.\n\nPyVLX enables you to run scenes and or open and close velux windows.",
+    long_description=README,
     author='Julius Mittenzwei',
     author_email='julius@mittenzwei.com',
     license='LGPL',
