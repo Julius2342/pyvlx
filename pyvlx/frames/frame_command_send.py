c0e1b40a0f051556a9f5fb1c79f214eec539806a
Julius Mittenzwei <julius@mittenzwei.com>
2018-12-15 15:37:17 +0100

    fixed some comments within frame_command_send

--- pyvlx/frames/frame_command_send.py
+++ pyvlx/frames/frame_command_send.py
@@ -27,20 +27,21 @@ class FrameCommandSendRequest(FrameBase):
         ret = bytes([self.session_id >> 8 & 255, self.session_id & 255])
         ret += bytes([self.originator.value])
         ret += bytes([self.priority.value])
-        # Parameter active
-        ret += bytes([0])
+        ret += bytes([0])  # ParameterActive pointing to main parameter (MP)
         # FPI 1+2
         ret += bytes([0])
         ret += bytes([0])
         # Main parameter + functional parameter (in our case: position)
         ret += bytes(self.position)
         ret += bytes(32)
+
         # Nodes array: Number of nodes + node array + padding
         ret += bytes([len(self.node_ids)])  # index array count
         ret += bytes(self.node_ids) + bytes(20-len(self.node_ids))
-        # Pririty Level Lock
+
+        # Priority  Level Lock
         ret += bytes([0])
-        # PLI 1+2
+        # Priority Level information 1+2
         ret += bytes([0, 0])
         # Locktime
         ret += bytes([0])
