3f52e63b2b3c33c7075d41b43b243b20d0b48b5e
Julius Mittenzwei <julius@mittenzwei.com>
2018-12-19 18:36:13 +0100

    Handling update frames with no node existing

--- pyvlx/node_updater.py
+++ pyvlx/node_updater.py
@@ -1,6 +1,7 @@
 """Module for updating nodes via frames."""
 from .frames import FrameNodeStatePositionChangedNotification
 from .opening_device import OpeningDevice
+from .log import PYVLXLOG
 
 
 class NodeUpdater():
@@ -13,7 +14,11 @@ class NodeUpdater():
     async def process_frame(self, frame):
         """Update nodes via frame, usually received by house monitor."""
         if isinstance(frame, FrameNodeStatePositionChangedNotification):
-            node = self.pyvlx.nodes[frame.node_id]
-            if isinstance(node, OpeningDevice):
-                node.position = frame.current_position
-                await node.after_update()
+            try:
+                node = self.pyvlx.nodes[frame.node_id]
+            except IndexError:
+                PYVLXLOG.warning("Could not access node with id %i", frame.node_id)
+            else:
+                if isinstance(node, OpeningDevice):
+                    node.position = frame.current_position
+                    await node.after_update()
