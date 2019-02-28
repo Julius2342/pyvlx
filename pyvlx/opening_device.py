f16c180f0da0e2a4ea06b90ca9f72e80ef399864
Julius Mittenzwei <julius@mittenzwei.com>
2019-02-28 09:38:55 +0100

    Expose (and explain) wait_for_completion parameter within Scene() and OpeningDevice() objects

--- pyvlx/opening_device.py
+++ pyvlx/opening_device.py
@@ -9,36 +9,89 @@ class OpeningDevice(Node):
     """Meta class for opening device with one main parameter for position."""
 
     def __init__(self, pyvlx, node_id, name):
-        """Initialize opening device."""
+        """Initialize opening device.
+
+        Parameters:
+            * pyvlx: PyVLX object
+            * node_id: internal id for addressing nodes.
+                Provided by KLF 200 device
+            * name: node name
+
+        """
         super().__init__(pyvlx=pyvlx, node_id=node_id, name=name)
         self.position = Position()
 
-    async def set_position(self, position):
-        """Set window to desired position."""
-        command_send = CommandSend(pyvlx=self.pyvlx, node_id=self.node_id, parameter=position)
+    async def set_position(self, position, wait_for_completion=True):
+        """Set window to desired position.
+
+        Parameters:
+            * position: Position object containing the target position.
+            * wait_for_completion: If set, function will return
+                after device has reached target position.
+
+        """
+        command_send = CommandSend(
+            pyvlx=self.pyvlx,
+            wait_for_completion=wait_for_completion,
+            node_id=self.node_id,
+            parameter=position)
         await command_send.do_api_call()
         if not command_send.success:
             raise PyVLXException("Unable to send command")
         await self.after_update()
 
-    async def open(self):
-        """Open window."""
-        await self.set_position(Position(position_percent=0))
+    async def open(self, wait_for_completion=True):
+        """Open window.
 
-    async def close(self):
-        """Close window."""
-        await self.set_position(Position(position_percent=100))
+        Parameters:
+            * wait_for_completion: If set, function will return
+                after device has reached target position.
 
-    async def stop(self):
-        """Stop window."""
-        await self.set_position(CurrentPosition())
+        """
+        await self.set_position(
+            position=Position(position_percent=0),
+            wait_for_completion=wait_for_completion)
+
+    async def close(self, wait_for_completion=True):
+        """Close window.
+
+        Parameters:
+            * wait_for_completion: If set, function will return
+                after device has reached target position.
+
+        """
+        await self.set_position(
+            position=Position(position_percent=100),
+            wait_for_completion=wait_for_completion)
+
+    async def stop(self, wait_for_completion=True):
+        """Stop window.
+
+        Parameters:
+            * wait_for_completion: If set, function will return
+                after device has reached target position.
+
+        """
+        await self.set_position(
+            position=CurrentPosition(),
+            wait_for_completion=wait_for_completion)
 
 
 class Window(OpeningDevice):
     """Window object."""
 
     def __init__(self, pyvlx, node_id, name, rain_sensor=False):
-        """Initialize Window class."""
+        """Initialize Window class.
+
+        Parameters:
+            * pyvlx: PyVLX object
+            * node_id: internal id for addressing nodes.
+                Provided by KLF 200 device
+            * name: node name
+            * rain_sensor: set if device is equipped with a
+                rain sensor.
+
+        """
         super().__init__(pyvlx=pyvlx, node_id=node_id, name=name)
         self.rain_sensor = rain_sensor
 
--- pyvlx/scene.py
+++ pyvlx/scene.py
@@ -7,14 +7,31 @@ class Scene:
     """Object for scene."""
 
     def __init__(self, pyvlx, scene_id, name):
-        """Initialize Scene object."""
+        """Initialize Scene object.
+
+        Parameters:
+            * pyvlx: PyVLX object
+            * scene_id: internal id for addressing scenes.
+                Provided by KLF 200 device
+            * name: scene name
+
+        """
         self.pyvlx = pyvlx
         self.scene_id = scene_id
         self.name = name
 
-    async def run(self):
-        """Run scene."""
-        activate_scene = ActivateScene(pyvlx=self.pyvlx, scene_id=self.scene_id)
+    async def run(self, wait_for_completion=True):
+        """Run scene.
+
+        Parameters:
+            * wait_for_completion: If set, function will return
+                after device has reached target position.
+
+        """
+        activate_scene = ActivateScene(
+            pyvlx=self.pyvlx,
+            wait_for_completion=wait_for_completion,
+            scene_id=self.scene_id)
         await activate_scene.do_api_call()
         if not activate_scene.success:
             raise PyVLXException("Unable to activate scene")
