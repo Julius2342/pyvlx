ce7c552ea943d65f394813e307985db5aa8ba5f7
Julius Mittenzwei <julius@mittenzwei.com>
2019-04-15 18:02:37 +0200

    safe_load is the safe way to do it. See: https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load(input)-Deprecation

--- pyvlx/config.py
+++ pyvlx/config.py
@@ -24,7 +24,7 @@ class Config:
         PYVLXLOG.info('Reading config file: %s', path)
         try:
             with open(path, 'r') as filehandle:
-                doc = yaml.load(filehandle)
+                doc = yaml.safe_load(filehandle)
                 self.test_configuration(doc, path)
                 self.host = doc['config']['host']
                 self.password = doc['config']['password']
