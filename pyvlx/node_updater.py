5e81f3f8e5a4657ae5339cff3240a316cf86b3da
Dimitri23 <dimitri.branthonne@gmail.com>
2020-03-28 22:41:29 +0100

    Update intensity property of a Ligth node

--- pyvlx/node_updater.py
+++ pyvlx/node_updater.py
@@ -3,7 +3,8 @@ from .frames import (
     FrameGetAllNodesInformationNotification,
     FrameNodeStatePositionChangedNotification)
 from .opening_device import OpeningDevice
-from .parameter import Position
+from .lightening_device import LighteningDevice
+from .parameter import Intensity, Position
 
 
 class NodeUpdater():
@@ -28,4 +29,7 @@ class NodeUpdater():
             node = self.pyvlx.nodes[frame.node_id]
             if isinstance(node, OpeningDevice):
                 node.position = Position(frame.current_position)
+                await node.after_update()
+            elif isinstance(node, LighteningDevice):
+                node.intensity = Intensity(frame.current_position)
                 await node.after_update()
