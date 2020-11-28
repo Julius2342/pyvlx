8aaf047ec944233fee0bff199c7768031588070c
icommitdesnet <georg.ritter@gmx.at>
2020-11-28 22:20:11 +0100

    fix pylint warnings

--- test/frame_get_system_table_update_ntf_test.py
+++ test/frame_get_system_table_update_ntf_test.py
@@ -7,7 +7,10 @@ class TestFrameGetSystemTableUpdateNotification(unittest.TestCase):
     """Test class for FrameGetSystemTableDataNotification."""
     # pylint: disable=too-many-public-methods,invalid-name
     maxDiff = None
-    EXAMPLE_PAYLOAD = bytes.fromhex('0102040810204080010204081020408001020408102040800102FF00000000000000000000000000000000000000000000000000')
+    EXAMPLE_PAYLOAD = bytes.fromhex(
+        '0102040810204080010204081020408001020408102040800102'
+        'FF00000000000000000000000000000000000000000000000000'
+        )
 
     def test_frame_from_payload(self):
         """Test parse FrameGetSystemTableDataNotification from raw."""
@@ -18,7 +21,7 @@ class TestFrameGetSystemTableUpdateNotification(unittest.TestCase):
                                                91, 100, 109, 118, 127, 128, 137, 146, 155,
                                                164, 173, 182, 191, 192, 201])
         self.assertTrue(frame.removednodeids == [0, 1, 2, 3, 4, 5, 6, 7])
-        
+
     def test_str(self):
         """Test string representation of FrameGetSystemTableDataNotification."""
         frame = FrameGetSystemTableUpdateNotification()
