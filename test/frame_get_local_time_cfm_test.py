56e511b450b5e86c4308953b13e18c2395996bdf
Georg R <39526093+icommitdesnet@users.noreply.github.com>
2020-12-01 09:05:39 +0100

    Update frame_get_local_time_cfm_test.py
    Remove whitespace

--- test/frame_get_local_time_cfm_test.py
+++ test/frame_get_local_time_cfm_test.py
@@ -7,7 +7,7 @@ from pyvlx.api.frames import FrameGetLocalTimeConfirmation
 
 class TestFrameGetLocalTimeConfirmation(unittest.TestCase):
     """Test class for FrameGetLocalTimeConfirmation."""
-    
+
     # pylint: disable=too-many-public-methods,invalid-name
 
     def test_bytes(self):
