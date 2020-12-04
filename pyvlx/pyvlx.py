2a6912e9f11935c92c343e9ac4fe5b0625ad57ca
icommitdesnet <georg.ritter@gmx.at>
2020-12-04 15:34:47 +0100

    remove duplicate getnetworksetup

--- pyvlx/pyvlx.py
+++ pyvlx/pyvlx.py
@@ -51,7 +51,6 @@ class PyVLX:
         )
 
         await self.klf200.get_state()
-        await self.klf200.get_network_setup()
         await self.klf200.set_utc()
         await self.klf200.get_network_setup()
         await house_status_monitor_enable(pyvlx=self)
