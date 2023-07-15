425e95220caa84a863272785966313aa00886ad1
Paul Daumlechner <paul.daumlechner@live.de>
2023-07-15 18:22:11 +0200

    Update pyvlx.py

--- pyvlx/pyvlx.py
+++ pyvlx/pyvlx.py
@@ -34,7 +34,7 @@ class PyVLX:
         self.version = None
         self.protocol_version = None
         self.klf200 = Klf200Gateway(pyvlx=self)
-        PYVLXLOG.debug("Loading https://github.com/pawlizio/pyvlx.git@Dual_roller_shutter v0.1.54")
+        PYVLXLOG.debug("Loading https://github.com/pawlizio/pyvlx.git@Dual_roller_shutter v0.1.55")
 
     async def connect(self):
         """Connect to KLF 200."""
