113f2a62ef90bbfd805cff21c83de69c9c1ac9a8
Paul Daumlechner <paul.daumlechner@live.de>
2023-12-31 00:27:31 +0100

    Version update for testing

--- pyvlx/pyvlx.py
+++ pyvlx/pyvlx.py
@@ -50,7 +50,7 @@ class PyVLX:
         self.protocol_version = None
         self.klf200 = Klf200Gateway(pyvlx=self)
         self.api_call_semaphore = asyncio.Semaphore(1)  # Limit parallel commands
-        PYVLXLOG.debug("Loadig pyvlx v0.1.78")
+        PYVLXLOG.debug("Loadig pyvlx v0.1.79")
 
     async def connect(self) -> None:
         """Connect to KLF 200."""
