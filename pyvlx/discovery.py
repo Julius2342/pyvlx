98842bd0d7a19baf7dbb04ee360c47c7e18781b3
Paul Daumlechner <paul.daumlechner@live.de>
2024-01-29 11:30:45 +0100

    Add zeroconf discovery support (#369)
    * Get version control for updates
    
    * Add discovery service
    
    * Add missing docstring.
    
    * Restore version
    
    * Add missing docstring
    
    * Add blank line
    
    * Add white line
    
    * fix isort
    
    * Update requirements
    
    * Update discovery.oy
    
    * Update python_support
    
    * Revert "Update python_support"
    
    This reverts commit 2ddeb0cf68ad68876c81b96b9c8a122ddd89561a.
    
    * Update discovery.py
    
    * Update coverage
    
    * Fix flake8
    
    * Cancel support for python 3.10
    
    * Update docstring
    
    * Fix isort
    
    * Avoid CancellationError
    
    * Remove trailing whitespace
    
    * isort fix
    
    * Add zeroconf to install_requires

---
+++ pyvlx/discovery.py
@@ -0,0 +1,87 @@
+"""Module to discover Velux KLF200 devices on the network."""
+import asyncio
+from asyncio import Event, Future, Task
+from dataclasses import dataclass
+from typing import Any, Optional
+
+from zeroconf import IPVersion
+from zeroconf.asyncio import (
+    AsyncServiceBrowser, AsyncServiceInfo, AsyncZeroconf)
+
+SERVICE_STARTS_WITH: str = "VELUX_KLF_LAN"
+SERVICE_TYPE: str = "_http._tcp.local."
+
+
+@dataclass
+class VeluxHost():
+    """Class to store Velux KLF200 host information."""
+
+    hostname: str
+    ip_address: str
+
+
+class VeluxDiscovery():
+    """Class to discover Velux KLF200 devices on the network."""
+
+    def __init__(self, zeroconf: AsyncZeroconf,) -> None:
+        """Initialize VeluxDiscovery object."""
+        self.zc: AsyncZeroconf = zeroconf
+        self.hosts: list[VeluxHost | None] = []
+        self.infos: list[AsyncServiceInfo | None] = []
+
+    async def _async_discover_hosts(self, min_wait_time: float, expected_hosts: int | None) -> None:
+        """Listen for zeroconf ServiceInfo."""
+        self.hosts.clear()
+        service_names: list[str] = []
+        tasks: list[Task] = []
+        got_host: Event = Event()
+
+        def add_info_and_host(fut: Future) -> None:
+            info: AsyncServiceInfo = fut.result()
+            self.infos.append(info)
+            host = VeluxHost(
+                hostname=info.name.replace("._http._tcp.local.", ""),
+                ip_address=info.parsed_addresses(version=IPVersion.V4Only)[0],
+            )
+            self.hosts.append(host)
+            got_host.set()
+
+        def handler(name: str, **kwargs: Any) -> None:  # pylint: disable=W0613:unused-argument
+            if name.startswith(SERVICE_STARTS_WITH):
+                if name not in service_names:
+                    service_names.append(name)
+                    task = asyncio.create_task(self.zc.async_get_service_info(type_=SERVICE_TYPE, name=name))
+                    task.add_done_callback(add_info_and_host)
+                    tasks.append(task)
+
+        browser: AsyncServiceBrowser = AsyncServiceBrowser(self.zc.zeroconf, SERVICE_TYPE, handlers=[handler])
+        if expected_hosts:
+            while len(self.hosts) < expected_hosts:
+                await got_host.wait()
+                got_host.clear()
+        while not self.hosts:
+            await asyncio.sleep(min_wait_time)
+        await browser.async_cancel()
+        await asyncio.gather(*tasks)
+
+    async def async_discover_hosts(
+        self,
+        timeout: float = 10,
+        min_wait_time: float = 3,
+        expected_hosts: Optional[int] = None
+    ) -> bool:
+        """Return true if Velux KLF200 devices found on the network.
+
+        This function creates a zeroconf AsyncServiceBrowser and
+        waits min_wait_time (seconds) for ServiceInfos from hosts.
+        Some devices may take some time to respond (i.e. if they currently have a high CPU load).
+        If one or more Hosts are found, the function cancels the ServiceBrowser and returns true.
+        If expected_hosts is set, the function ignores min_wait_time and returns true once expected_hosts are found.
+        If timeout (seconds) is exceeded, the function returns false.
+        """
+        try:
+            async with asyncio.timeout(timeout):
+                await self._async_discover_hosts(min_wait_time, expected_hosts)
+        except TimeoutError:
+            return False
+        return True
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os import path
 
 from setuptools import find_packages, setup
 
-REQUIRES = ["PyYAML"]
+REQUIRES = ["PyYAML", "zeroconf"]
 
 PKG_ROOT = os.path.dirname(__file__)
 
@@ -36,7 +36,6 @@ setup(
         "Intended Audience :: Developers",
         "Topic :: System :: Hardware :: Hardware Drivers",
         "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
-        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
     ],
@@ -44,7 +43,7 @@ setup(
     package_data={
         "pyvlx": ["py.typed"],
     },
-    python_requires='>=3.10',
+    python_requires='>=3.11',
     install_requires=REQUIRES,
     keywords="velux KLF 200 home automation",
     zip_safe=False,
