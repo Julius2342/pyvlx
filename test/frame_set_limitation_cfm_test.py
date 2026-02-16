4b52483d674feb9eccdab85d792c7f173a4aef62
Tobias Jähnel <tjaehnel@gmail.com>
2026-02-16 23:59:55 +0100

    Fixed usage of constant in test.

--- test/frame_set_limitation_cfm_test.py
+++ test/frame_set_limitation_cfm_test.py
@@ -17,19 +17,19 @@ class TestFrameSetLimitationConfirmation(unittest.TestCase):
     def test_bytes(self):
         """Test FrameSetLimitationConfirmation bytes."""
         frame = FrameSetLimitationConfirmation(session_id=1, status=SetLimitationRequestStatus.ACCEPTED)
-        self.assertEqual(bytes(frame), b'\x00\x06\x03\x11\x00\x01\x01\x14')
+        self.assertEqual(bytes(frame), self.EXAMPLE_FRAME_ACCEPTED)
 
         frame = FrameSetLimitationConfirmation(session_id=2, status=SetLimitationRequestStatus.REJECTED)
-        self.assertEqual(bytes(frame), b'\x00\x06\x03\x11\x00\x02\x00\x16')
+        self.assertEqual(bytes(frame), self.EXAMPLE_FRAME_REJECTED)
 
     def test_frame_from_raw(self):
         """Test parse FrameGetLimitationStatusConfirmation from raw."""
-        frame = frame_from_raw(b'\x00\x06\x03\x11\x00\x01\x01\x14')
+        frame = frame_from_raw(self.EXAMPLE_FRAME_ACCEPTED)
         self.assertTrue(isinstance(frame, FrameSetLimitationConfirmation))
         self.assertEqual(frame.session_id, 1)
         self.assertEqual(frame.status, SetLimitationRequestStatus.ACCEPTED)
 
-        frame = frame_from_raw(b'\x00\x06\x03\x11\x00\x02\x00\x16')
+        frame = frame_from_raw(self.EXAMPLE_FRAME_REJECTED)
         self.assertTrue(isinstance(frame, FrameSetLimitationConfirmation))
         self.assertEqual(frame.session_id, 2)
         self.assertEqual(frame.status, SetLimitationRequestStatus.REJECTED)
