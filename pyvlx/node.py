05a6305697935f5b78c8197d29c2a71ddb61b912
Ingmar Stein <490610+IngmarStein@users.noreply.github.com>
2026-02-24 22:35:43 +0100

    Add last_frame_status_reply property to Node class (#608)
    * Add status_reply property to Node class
    
    This introduces the status_reply property to the base Node class and updates the NodeUpdater to capture it from FrameStatusRequestNotification frames. It also guarantees that node.after_update() is called when the status changes to ensure downstream applications like Home Assistant are notified of changes such as low battery status.
    
    It improves NodeUpdater by removing unnecessary after_update() calls when nothing actually changed. 
    
    ---------
    
    Co-authored-by: Wolfgang Miller-Reichling <wolfgang@miller-reichling.de>

--- pyvlx/node.py
+++ pyvlx/node.py
@@ -8,7 +8,7 @@ and roller shutters.
 from typing import TYPE_CHECKING, Any, Awaitable, Callable, List, Optional
 
 from .api import SetNodeName
-from .const import OperatingState
+from .const import OperatingState, StatusReply
 from .exception import PyVLXException
 
 if TYPE_CHECKING:
@@ -27,6 +27,7 @@ class Node:
         self.name = name
         self.serial_number = serial_number
         self.last_frame_state: Optional[OperatingState] = None
+        self.last_frame_status_reply: Optional[StatusReply] = None
         self.device_updated_cbs: List[CallbackType] = []
         self.pyvlx.connection.register_connection_opened_cb(self.after_update)
         self.pyvlx.connection.register_connection_closed_cb(self.after_update)
@@ -61,11 +62,11 @@ class Node:
     def __str__(self) -> str:
         """Return object as readable string."""
         return (
-            '<{} name="{}" '
-            'node_id="{}" '
-            'serial_number="{}"/>'.format(
-                type(self).__name__, self.name, self.node_id, self.serial_number
-            )
+            f'<{type(self).__name__} name="{self.name}" '
+            f'node_id="{self.node_id}" '
+            f'serial_number="{self.serial_number}" '
+            f'last_frame_state="{self.last_frame_state}" '
+            f'last_frame_status_reply="{self.last_frame_status_reply}"/>'
         )
 
     def __eq__(self, other: Any) -> bool:
