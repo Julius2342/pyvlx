05fdd149e8186c85bf5ed6e55c48ea9291548a40
Wolfgang Miller-Reichling <wolfgang@miller-reichling.de>
2026-02-27 14:30:29 +0100

    replace manual timeout handling with asyncio.timeout

--- pyvlx/api/api_event.py
+++ pyvlx/api/api_event.py
@@ -1,6 +1,6 @@
-"""Base class for waiting for a specific answer frame from velux ap.."""
+"""Base class for waiting for a specific answer frame from Velux API."""
 import asyncio
-from typing import TYPE_CHECKING, Optional
+from typing import TYPE_CHECKING
 
 from .frames import FrameBase
 
@@ -9,7 +9,11 @@ if TYPE_CHECKING:
 
 
 class ApiEvent:
-    """Base class for waiting a specific frame from API connection."""
+    """Base class for waiting a specific frame from API connection.
+
+    Objects of this class are single-use only, i.e. after one
+    call to do_api_call() they must be discarded.
+    """
 
     def __init__(self, pyvlx: "PyVLX", timeout_in_seconds: int = 10):
         """Initialize ApiEvent."""
@@ -18,11 +22,14 @@ class ApiEvent:
 
         self.success = False
         self.timeout_in_seconds = timeout_in_seconds
-        self.timeout_callback = None
-        self.timeout_handle: Optional[asyncio.TimerHandle] = None
+
+        self.used = False
 
     async def do_api_call(self) -> None:
         """Start. Sending and waiting for answer."""
+        assert not self.used, "ApiEvent objects are single-use only"
+        self.used = True
+
         # We check for connection before entering the semaphore section
         # because otherwise we might try to connect, which calls this, and we get stuck on
         # the semaphore.
@@ -31,17 +38,27 @@ class ApiEvent:
         if self.pyvlx.get_connected():
             async with self.pyvlx.api_call_semaphore:
                 self.pyvlx.connection.register_frame_received_cb(self.response_rec_callback)
-                await self.send_frame()
-                await self.start_timeout()
-                await self.response_received_or_timeout.wait()
-                self.response_received_or_timeout.clear()
-                await self.stop_timeout()
-                self.pyvlx.connection.unregister_frame_received_cb(self.response_rec_callback)
+                try:
+                    await self.send_frame()
+                    try:
+                        async with asyncio.timeout(self.timeout_in_seconds):
+                            await self.response_received_or_timeout.wait()
+
+                    except TimeoutError:
+                        self.success = False
+
+                finally:
+                    self.pyvlx.connection.unregister_frame_received_cb(self.response_rec_callback)
         else:
             self.success = False
 
     async def handle_frame(self, frame: FrameBase) -> bool:
-        """Handle incoming API frame, return True if this was the expected frame."""
+        """Handle incoming frame. Return True if this frame completes the API call.
+
+        Note that the return value only indicates if the API call is completed,
+        not if it was successful.
+        The success attribute has to be checked for that.
+        """
         raise NotImplementedError("handle_frame has to be implemented")
 
     async def send_frame(self) -> None:
@@ -50,24 +67,9 @@ class ApiEvent:
 
     def request_frame(self) -> FrameBase:
         """Construct initiating frame."""
-        raise NotImplementedError("send_frame has to be implemented")
+        raise NotImplementedError("request_frame has to be implemented")
 
     async def response_rec_callback(self, frame: FrameBase) -> None:
         """Handle frame. Callback from internal api connection."""
         if await self.handle_frame(frame):
             self.response_received_or_timeout.set()
-
-    def timeout(self) -> None:
-        """Handle timeout for not having received expected frame."""
-        self.response_received_or_timeout.set()
-
-    async def start_timeout(self) -> None:
-        """Start timeout."""
-        self.timeout_handle = self.pyvlx.connection.loop.call_later(
-            self.timeout_in_seconds, self.timeout
-        )
-
-    async def stop_timeout(self) -> None:
-        """Stop timeout."""
-        if self.timeout_handle is not None:
-            self.timeout_handle.cancel()
