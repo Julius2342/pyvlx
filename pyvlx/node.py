e1a33ad72211086a411b9bf49aa9def33a4a97bc
MaGeHome <155181202+MaGeHome@users.noreply.github.com>
2026-03-01 01:44:15 +0100

    add_import_node.py

--- pyvlx/node.py
+++ pyvlx/node.py
@@ -8,7 +8,7 @@ and roller shutters.
 from typing import TYPE_CHECKING, Any, Awaitable, Callable, List, Optional
 
 from .api import SetNodeName
-from .const import OperatingState, StatusReply
+from .const import OperatingState, RunStatus, StatusReply
 from .exception import PyVLXException
 
 if TYPE_CHECKING:
